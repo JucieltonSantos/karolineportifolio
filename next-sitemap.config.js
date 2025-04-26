6ca53da97401d57b9352a0a5f7d846044f564976
JucieltonSantos <jucielton_santos@hotmail.com>
2025-04-26 16:19:46 -0300

    ajustar url do site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://drakarolinemariamaria.com.br/",
+  siteUrl: "https://drakarolinemaria.com.br/",
   generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.7,
